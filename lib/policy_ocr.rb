8a74b6cca8dd08de930f50eb40e7045bff93d57e
Diego Klappenbach <dklappenbach@vairix.com>
2021-05-31 13:11:44 -0300

    Clean code

--- lib/policy_ocr.rb
+++ lib/policy_ocr.rb
@@ -1,16 +1,7 @@
 module PolicyOcr
-  def self.foo
+  def self.scan_files
     file = File.open("./spec/fixtures/sample.txt")
     file_data = file.readlines.map(&:chomp)
-    entry_number_1_list = [0,1,2,3,4,5,6,7,8,9]
-    entry_number_2_list = [0,1,2,3,4,5,6,7,8,9]
-    entry_number_3_list = [0,1,2,3,4,5,6,7,8,9]
-    entry_number_4_list = [0,1,2,3,4,5,6,7,8,9]
-    entry_number_5_list = [0,1,2,3,4,5,6,7,8,9]
-    entry_number_6_list = [0,1,2,3,4,5,6,7,8,9]
-    entry_number_7_list = [0,1,2,3,4,5,6,7,8,9]
-    entry_number_8_list = [0,1,2,3,4,5,6,7,8,9]
-    entry_number_9_list = [0,1,2,3,4,5,6,7,8,9]
 
     numbers = {
       entry_number_1_list: [0,1,2,3,4,5,6,7,8,9],
@@ -36,8 +27,9 @@ module PolicyOcr
         full_entry_number += get_number(numbers[:entry_number_7_list])
         full_entry_number += get_number(numbers[:entry_number_8_list])
         full_entry_number += get_number(numbers[:entry_number_9_list])
+        # full_entry_number = format_output(full_entry_number)
         full_entry_number += "\n"
-
+        puts full_entry_number
         File.open("./spec/fixtures/sample-result.txt", "a") { |f| f.write "#{full_entry_number}" }
 
         numbers = {
@@ -59,44 +51,35 @@ module PolicyOcr
             part.each_with_index do |number_part, ind|
               if ind == 0 #first part of element
                 if number_part == ' '
-                  entry_number_1_list = entry_number_1_list - []
                   numbers["entry_number_#{i+1}_list".to_sym] = numbers["entry_number_#{i+1}_list".to_sym] - []
                 end
                 if number_part == '|'
-                  entry_number_1_list = entry_number_1_list - [0,1,2,3,4,5,6,7,8,9]
                   numbers["entry_number_#{i+1}_list".to_sym] = numbers["entry_number_#{i+1}_list".to_sym] - [0,1,2,3,4,5,6,7,8,9]
                 end
                 if number_part == '_'
-                  entry_number_1_list = entry_number_1_list - [0,1,2,3,4,5,6,7,8,9]
                   numbers["entry_number_#{i+1}_list".to_sym] = numbers["entry_number_#{i+1}_list".to_sym] - [0,1,2,3,4,5,6,7,8,9]
                 end
               end
               
               if ind == 1 #second part of element
                 if number_part == ' '
-                  entry_number_1_list = entry_number_1_list - [0,2,3,5,6,7,8,9]
                   numbers["entry_number_#{i+1}_list".to_sym] = numbers["entry_number_#{i+1}_list".to_sym] - [0,2,3,5,6,7,8,9]
                 end
                 if number_part == '|'
-                  entry_number_1_list = entry_number_1_list - [0,1,2,3,4,5,6,7,8,9]
                   numbers["entry_number_#{i+1}_list".to_sym] = numbers["entry_number_#{i+1}_list".to_sym] - [0,1,2,3,4,5,6,7,8,9]
                 end
                 if number_part == '_'
-                  entry_number_1_list = entry_number_1_list - [1,4]
                   numbers["entry_number_#{i+1}_list".to_sym] = numbers["entry_number_#{i+1}_list".to_sym] - [1,4]
                 end
               end
               if ind == 2 #third part of element
                 if number_part == ' '
-                  entry_number_1_list = entry_number_1_list - []
                   numbers["entry_number_#{i+1}_list".to_sym] = numbers["entry_number_#{i+1}_list".to_sym] - []
                 end
                 if number_part == '|'
-                  entry_number_1_list = entry_number_1_list - [0,1,2,3,4,5,6,7,8,9]
                   numbers["entry_number_#{i+1}_list".to_sym] = numbers["entry_number_#{i+1}_list".to_sym] - [0,1,2,3,4,5,6,7,8,9]
                 end
                 if number_part == '_'
-                  entry_number_1_list = entry_number_1_list - [0,1,2,3,4,5,6,7,8,9]
                   numbers["entry_number_#{i+1}_list".to_sym] = numbers["entry_number_#{i+1}_list".to_sym] - [0,1,2,3,4,5,6,7,8,9]
                 end
               end
@@ -107,43 +90,34 @@ module PolicyOcr
             part.each_with_index do |number_part, ind|
               if ind == 0 #first part of element
                 if number_part == ' '
-                  entry_number_1_list = entry_number_1_list - [0,4,5,6,8,9]
                   numbers["entry_number_#{i+1}_list".to_sym] = numbers["entry_number_#{i+1}_list".to_sym] - [0,4,5,6,8,9]
                 end
                 if number_part == '|'
-                  entry_number_1_list = entry_number_1_list - [1,2,3,7]
                   numbers["entry_number_#{i+1}_list".to_sym] = numbers["entry_number_#{i+1}_list".to_sym] - [1,2,3,7]
                 end
                 if number_part == '_'
-                  entry_number_1_list = entry_number_1_list - [0,1,2,3,4,5,6,7,8,9]
                   numbers["entry_number_#{i+1}_list".to_sym] = numbers["entry_number_#{i+1}_list".to_sym] - [0,1,2,3,4,5,6,7,8,9]
                 end
               end
               if ind == 1 #second part of element
                 if number_part == ' '
-                  entry_number_1_list = entry_number_1_list - [2,3,4,5,6,8,9]
                   numbers["entry_number_#{i+1}_list".to_sym] = numbers["entry_number_#{i+1}_list".to_sym] - [2,3,4,5,6,8,9]
                 end
                 if number_part == '|'
-                  entry_number_1_list = entry_number_1_list - [0,1,2,3,4,5,6,7,8,9]
                   numbers["entry_number_#{i+1}_list".to_sym] = numbers["entry_number_#{i+1}_list".to_sym] - [0,1,2,3,4,5,6,7,8,9]
                 end
                 if number_part == '_'
-                  entry_number_1_list = entry_number_1_list - [0,1,7]
                   numbers["entry_number_#{i+1}_list".to_sym] = numbers["entry_number_#{i+1}_list".to_sym] - [0,1,7]
                 end
               end
               if ind == 2 #third part of element
                 if number_part == ' '
-                  entry_number_1_list = entry_number_1_list - [0,1,2,3,4,7,8,9]
                   numbers["entry_number_#{i+1}_list".to_sym] = numbers["entry_number_#{i+1}_list".to_sym] - [0,1,2,3,4,7,8,9]
                 end
                 if number_part == '|'
                   numbers["entry_number_#{i+1}_list".to_sym] = numbers["entry_number_#{i+1}_list".to_sym] - [5,6]
-                  entry_number_1_list = entry_number_1_list - [5,6]
                 end
                 if number_part == '_'
-                  entry_number_1_list = entry_number_1_list - [0,1,2,3,4,5,6,7,8,9]
                   numbers["entry_number_#{i+1}_list".to_sym] = numbers["entry_number_#{i+1}_list".to_sym] - [0,1,2,3,4,5,6,7,8,9]
                 end
               end
@@ -155,44 +129,35 @@ module PolicyOcr
             part.each_with_index do |number_part, ind|
               if ind == 0 #first part of element
                 if number_part == ' '
-                  entry_number_1_list = entry_number_1_list - [0,2,6,8]
                   numbers["entry_number_#{i+1}_list".to_sym] = numbers["entry_number_#{i+1}_list".to_sym] - [0,2,6,8]
                 end
                 if number_part == '|'
                   numbers["entry_number_#{i+1}_list".to_sym] = numbers["entry_number_#{i+1}_list".to_sym] - [1,3,4,5,7,9]
-                  entry_number_1_list = entry_number_1_list - [1,3,4,5,7,9]
                 end
                 if number_part == '_'
-                  entry_number_1_list = entry_number_1_list - [0,1,2,3,4,5,6,7,8,9]
                   numbers["entry_number_#{i+1}_list".to_sym] = numbers["entry_number_#{i+1}_list".to_sym] - [0,1,2,3,4,5,6,7,8,9]
                 end
               end
               if ind == 1 #second part of element
                 if number_part == ' '
-                  entry_number_1_list = entry_number_1_list - [0,2,3,5,6,8,9]
                   numbers["entry_number_#{i+1}_list".to_sym] = numbers["entry_number_#{i+1}_list".to_sym] - [0,2,3,5,6,8,9]
                 end
                 if number_part == '|'
-                  entry_number_1_list = entry_number_1_list - [0,1,2,3,4,5,6,7,8,9]
                   numbers["entry_number_#{i+1}_list".to_sym] = numbers["entry_number_#{i+1}_list".to_sym] - [0,1,2,3,4,5,6,7,8,9]
                 end
                 if number_part == '_'
-                  entry_number_1_list = entry_number_1_list - [1,4,7]
                   numbers["entry_number_#{i+1}_list".to_sym] = numbers["entry_number_#{i+1}_list".to_sym] - [1,4,7]
                 end
               end
               
               if ind == 2 #third part of element
                 if number_part == ' '
-                  entry_number_1_list = entry_number_1_list - [0,1,3,4,5,6,7,8,9]
                   numbers["entry_number_#{i+1}_list".to_sym] = numbers["entry_number_#{i+1}_list".to_sym] - [0,1,3,4,5,6,7,8,9]
                 end
                 if number_part == '|'
-                  entry_number_1_list = entry_number_1_list - [2]
                   numbers["entry_number_#{i+1}_list".to_sym] = numbers["entry_number_#{i+1}_list".to_sym] - [2]
                 end
                 if number_part == '_'
-                  entry_number_1_list = entry_number_1_list - [0,1,2,3,4,5,6,7,8,9]
                   numbers["entry_number_#{i+1}_list".to_sym] = numbers["entry_number_#{i+1}_list".to_sym] - [0,1,2,3,4,5,6,7,8,9]
                 end
               end
@@ -200,9 +165,8 @@ module PolicyOcr
           end
         end
       end
-      # puts element.length
     end
-    file_data.first #element.first
+    puts "scan completed"
   end
 
   private
@@ -223,6 +187,19 @@ module PolicyOcr
     end
 
     return total_sum % 11 == 0
+  end
+
+  def self.format_output(numbers)
+    if numbers.include?('?')
+      "#{numbers} - ILL"
+    else
+      formatted_numbers = numbers.split('').map { |num| num.to_i }
+      if validate_number(formatted_numbers)
+        numbers
+      else
+        "#{numbers} - ERR"
+      end
+    end
   end
   
 end
